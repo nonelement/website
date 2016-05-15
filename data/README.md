f60ef5c260828b6d0ef39c48116f61942c362550
Anthony Kirkpatrick <anthony.j.kirkpatrick@gmail.com>
2016-05-14 21:26:20 -0400

    Removed useless files, added gulp

--- data/README.md
+++
@@ -1,3 +0,0 @@
-This directory contains a few bits of data that some apps use.
-
-Largely it's just sample data that I've used in when messing around with different ideas.
--- js/base.js
+++ js/base.js
@@ -24,9 +24,3 @@
 function isDebug() {
 	return typeof DEBUG !== "undefined" && DEBUG;
 }
-
-console.log("Hello! The script that prints this message is currently the only one on the page.");
-console.log("You might wonder why I put a script on this page that does basically nothing.");
-console.log("Well... I got nothing for you. Yet. But there will be things here in the future.");
-console.log("I've got some ideas. A few decent ones at least.");
-console.log("Check back often for changes. :: AK (If you find yourself wanting to speak with me, send a few lines to anthony.j.kirkpatrick@gmail.com)");
